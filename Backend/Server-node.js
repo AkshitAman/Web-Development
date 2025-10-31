7c390c04b82834e0e8e65299541a97aa7fcc9908
Akshit Aman <amanakshit1234@gmail.com>
2025-11-01 00:15:58 +0530

    Modified Code for Creating a web server

--- Backend/Server-node.js
+++ Backend/Server-node.js
@@ -4,13 +4,21 @@ const hostname = "127.0.0.1";
 const port = 7000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello There!");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello There!");
+  } else if (req.url === "/Welcome") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Thanks for Visiting Here ! Hope You may have a good Time..");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 Not Found");
+  }
 });
 
-
-server.listen(port,hostname,()=>{
-    console.log(`Server is Listening at http://${hostname}:${port}`);
-    
-})
+server.listen(port, hostname, () => {
+  console.log(`Server is Listening at http://${hostname}:${port}`);
+});
