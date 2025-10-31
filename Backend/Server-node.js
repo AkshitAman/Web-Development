01f263118d58c6b0dd17ef55ec6cf8e26c1098f2
Akshit Aman <amanakshit1234@gmail.com>
2025-10-31 23:56:37 +0530

    Learned to create a web server

---
+++ Backend/Server-node.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 7000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello There!");
+});
+
+
+server.listen(port,hostname,()=>{
+    console.log(`Server is Listening at http://${hostname}:${port}`);
+    
+})
