ffae405602431088897894e9629793b80b20bea4
Akshit Aman <amanakshit1234@gmail.com>
2025-11-23 23:38:26 +0530

    Learned to create our own React library and JSX

--- React/01_basicvite/src/App.jsx
+++ React/01_basicvite/src/App.jsx
@@ -4,12 +4,13 @@ import Youtube from "./youtube.jsx"
 
 
 function App() {
-
+  const username = 'Akshit';
 
   return (
+    
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2+2}</h1>
+      <h1>Vite react app {username}</h1>
       <Youtube/>
     </>
   )
--- React/01_basicvite/src/main.jsx
+++ React/01_basicvite/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  Children: "Click me to visit google",
+};
 
-    <App />
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
 
+const AnotherElement = (
+    <a href="http://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'http://google.com', target:'_blank'},  // Second one is always an object
+    'Click to visit Google'
+)
+
+createRoot(document.getElementById('root')).render(    // React.render only accept one Argument
+
+    // MyApp() it will create render issue instead of that we will do 
+    // <MyApp /> 
+    // AnotherElement
+    // areactElement
+    <App/>
 )
