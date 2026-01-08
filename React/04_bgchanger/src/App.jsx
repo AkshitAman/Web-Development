5b6c7e0b6e4ae9d72687d84794e57ab641ff322e
Akshit Aman <amanakshit1234@gmail.com>
2026-01-09 00:03:06 +0530

    Learned to change background color

---
+++ React/04_bgchanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('blue')
+
+  // function changeColor(color) {
+  //   setColor(color)
+  // }
+
+return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <button
+          onClick={()=> setColor('black')}
+           className='bg-white outline-none px-4 py-1 rounded-full shadow-lg text-black'>Dark Mode</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
