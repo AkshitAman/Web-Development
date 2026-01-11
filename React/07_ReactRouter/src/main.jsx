4f1b7f49a251ffc6a2b75b619b6ed8b4b3a77945
Akshit Aman <amanakshit1234@gmail.com>
2026-01-11 18:28:03 +0530

    Learning React Router

---
+++ React/07_ReactRouter/src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import Layout from "./layout.jsx"
+import Home from "./Components/Home/Home.jsx"
+import About from "./Components/About/About.jsx"
+import Contact_Us from "./Components/Contact Us/Contact_Us.jsx"
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path ='' element={<Home />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact_Us />}/>
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
